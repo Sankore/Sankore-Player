c2bb4609294bc6858bd67f5f98714ec6d401d2ef
matthieu.rudaz <matthieu.rudaz@mnemis.com>
2010-02-26 10:12:16 +0100

    set the controller for wiktionary up

--- app/controllers/wikibot_controller.rb
+++ app/controllers/wikibot_controller.rb
@@ -8,24 +8,27 @@ class WikibotController < ActionController::Base
     agent = Mechanize.new { |a| a.user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_2; de-at) AppleWebKit/531.21.8 (KHTML, like Gecko) Version/4.0.4 Safari/531.21.10 info@mnemis.com' }
     
     if @mode == 'wiki'
-      @surl = "http://" + @lang +".m.wikipedia.org/wiki/" + @input;
+      @surl = "http://" + @lang +".m.wikipedia.org/wiki/" + @input
       page = agent.get(@surl)
       
-      @reqUrl = '<a href="http://' + @lang + '.m.wikipedia.org/wiki/';
-      @fixImgLinks = page.body.gsub('<a href="/wiki', @reqUrl);
-      @wikibuttons = '<img src="http://' + @lang + '.m.wikipedia.org/skins-1.5/common/images/';
-      @fixButtons = @fixImgLinks.gsub('<img src="/skins-1.5/common/images/', @wikibuttons);
-      @fixButtons = @fixButtons.gsub('SRC="/javascripts/', 'SRC="http://m.wikipedia.com/javascripts/');
-      @fixButtons = @fixButtons.gsub("<link href='/", "<link href='http://m.wikipedia.com/");
-      @fixButtons.insert(@fixButtons.index("</head>"), '<link href="/widgets/wikipedia/wikipedia/stylesheets/wiki.css" media="all" rel="Stylesheet" type="text/css" />');
+      @reqUrl = '<a href="http://' + @lang + '.m.wikipedia.org/wiki/'
+      @fixImgLinks = page.body.gsub('<a href="/wiki', @reqUrl)
+      @wikibuttons = '<img src="http://' + @lang + '.m.wikipedia.org/skins-1.5/common/images/'
+      @fixButtons = @fixImgLinks.gsub('<img src="/skins-1.5/common/images/', @wikibuttons)
+      @fixButtons = @fixButtons.gsub('SRC="/javascripts/', 'SRC="http://m.wikipedia.com/javascripts/')
+      @fixButtons = @fixButtons.gsub("<link href='/", "<link href='http://m.wikipedia.com/")
+      @fixButtons.insert(@fixButtons.index("</head>"), '<link href="/widgets/wikipedia/wikipedia/stylesheets/wiki.css" media="all" rel="Stylesheet" type="text/css" />')
     elsif @mode == 'wiktionary'
-      @surl = "http://" + @lang +".wiktionary.org/wiki/Special:Search/" + @input;
+      @surl = "http://" + @lang +".m.wiktionary.org/wiki/" + @input
       page = agent.get(@surl)
       
-      @reqUrl = '<a href="http://' + @lang + '.m.wiktionary.org/wiki/Special:Search'
-      @fixImgLinks = page.body.gsub('<a href="/wiki', @reqUrl);
+      @reqUrl = '<a href="http://' + @lang + '.m.wiktionary.org/wiki/'
+      @fixImgLinks = page.body.gsub('<a href="/wiki', @reqUrl)
       @wikibuttons = '<img src="http://' + @lang + '.m.wiktionary.org/skins-1.5/common/images/'
-      @fixButtons = @fixImgLinks.gsub('<img src="/skins-1.5/common/images/', @wikibuttons);
+      @fixButtons = @fixImgLinks.gsub('<img src="/skins-1.5/common/images/', @wikibuttons)
+      @fixButtons = @fixButtons.gsub('SRC="/javascripts/', 'SRC="http://m.wiktionary.com/javascripts/')
+      @fixButtons = @fixButtons.gsub("<link href='/", "<link href='http://m.wiktionary.com/")
+      @fixButtons.insert(@fixButtons.index("</head>"), '<link href="/widgets/wiktionary/wiktionary/stylesheets/wiki.css" media="all" rel="Stylesheet" type="text/css" />')
     end
     @content = @fixButtons
   end
