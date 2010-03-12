b253a201f651b1a1975e7a774c98f7281286fc8a
matthieu.rudaz <matthieu.rudaz@mnemis.com>
2010-03-12 15:16:01 +0100

    remove protect_from_forgery

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect 'widgets/wikibot/search', :controller => 'wikibot', :action => 'search', :conditions => { :method => :get }
   map.connect 'widgets/edu-game', :controller => 'WidgetResult', :action => 'record_result', :conditions => { :method => :post}
-  map.connect 'widgets/edu-game', :controller => 'WidgetResult', :action => 'record_result', :conditions => { :method => :get}
   
   map.connect 'b46eef20-d35a-11de-8a39-0800200c9a66/personal', :controller => 'license', :action => 'generate', :conditions => { :method => :get}, :license_type => 'personal'
   map.connect 'b46eef20-d35a-11de-8a39-0800200c9a66/workstation', :controller => 'license', :action => 'generate', :conditions => { :method => :get}, :license_type => 'workstation'  
