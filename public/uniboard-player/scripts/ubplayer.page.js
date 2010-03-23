2045101edd9ca49da0b12ef73f0bfbab1f3a0a94
matthieu.rudaz <matthieu.rudaz@mnemis.com>
2010-03-23 12:40:23 +0100

    Uniboard player object apps viewer

--- public/uniboard-player/scripts/ubplayer.page.js
+++ public/uniboard-player/scripts/ubplayer.page.js
@@ -1,10 +1,10 @@
 
-var UBPage = {};
+UbPlayer.Page = {};
 
-UBPage.svgNS = 'http://www.w3.org/2000/svg';
-UBPage.uniboardNS = 'http://uniboard.mnemis.com/document';
+UbPlayer.Page.svgNS = 'http://www.w3.org/2000/svg';
+UbPlayer.Page.uniboardNS = 'http://uniboard.mnemis.com/document';
 
-UBPage.reduceDomain = function()
+UbPlayer.Page.reduceDomain = function()
 {
   var allDomainsParts = document.domain.split(".");
   
@@ -12,36 +12,48 @@ UBPage.reduceDomain = function()
     document.domain = allDomainsParts[allDomainsParts.length - 2] + "." + allDomainsParts[allDomainsParts.length - 1];
 }
 
-UBPage.updateForeignObjects = function()
+UbPlayer.Page.updateForeignObjects = function()
 {
   var foreignObjects = document.getElementsByTagName('foreignObject'); 
 
   for(var i = 0; i < foreignObjects.length; i++)
   {
     var fo = foreignObjects[i];
-    var outline = document.createElementNS(UBPage.svgNS, 'rect');
+    var outline = document.createElementNS(UbPlayer.Page.svgNS, 'rect');
+    var widgetUrl = window.location + '/../' + fo.getAttributeNS(UbPlayer.Page.uniboardNS, 'src');
+    var widgetUuid = fo.getAttributeNS(UbPlayer.Page.uniboardNS, 'uuid');
+    
+    outline.setAttributeNS(null, 'ry', 5);
+    outline.setAttributeNS(null, 'rx', 5);
     outline.setAttributeNS(null, 'x', fo.attributes['x'].value);
     outline.setAttributeNS(null, 'y', fo.attributes['y'].value - 20);
     outline.setAttributeNS(null, 'width', fo.attributes['width'].value);
-    outline.setAttributeNS(null, 'height', 20 /*fo.attributes['height'].value*/);
+    outline.setAttributeNS(null, 'height', fo.attributes['height'].value);
 
     outline.setAttributeNS(null, 'transform', fo.attributes['transform'].value);
-    outline.setAttributeNS(null, 'fill', '#D8D8D8');
+    outline.setAttributeNS(null, 'id', widgetUuid);
+    outline.setAttributeNS(null, 'class', 'out');
 
-    var widgetUrl = window.location + '/../' + fo.getAttributeNS(UBPage.uniboardNS, 'src') + "/index.html";
-    outline.setAttributeNS(null, 'onclick', "window.open('" + widgetUrl + "');");
+    outline.setAttributeNS(null, 'onclick', "window.parent.myUbPlayer.viewer.show('" + widgetUrl + "');");
+    outline.setAttributeNS(null, 'onmouseover', "UbPlayer.Page.highlight('" + widgetUuid + "')");
+    outline.setAttributeNS(null, 'onmouseout', "document.getElementById('" + widgetUuid + "').setAttributeNS(null, 'class', 'out')");
 
     foreignObjects[i].parentNode.insertBefore(outline, foreignObjects[i]);
   }
 }
 
-UBPage.init = function() 
+UbPlayer.Page.init = function() 
 {
-  UBPage.reduceDomain();
-  UBPage.updateForeignObjects();
+  UbPlayer.Page.reduceDomain();
+  UbPlayer.Page.updateForeignObjects();
 }
 
-window.onload = UBPage.init;
+UbPlayer.Page.highlight = function(widgetUuid)
+{
+  document.getElementById(widgetUuid).setAttributeNS(null, 'class', 'over');
+}
+
+window.onload = UbPlayer.Page.init;
 
 
 
