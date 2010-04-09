129e2a5bfd3eafe0b676313efb4e0704e80e9462
matthieu.rudaz <matthieu.rudaz@mnemis.com>
2010-04-09 16:04:20 +0200

    fixes

--- public/uniboard-player/scripts/ubplayer.js
+++ public/uniboard-player/scripts/ubplayer.js
@@ -4,7 +4,5 @@ UbPlayer.reduceDomain = function(){
   var allDomainsParts = document.domain.split(".");
   
   if (allDomainsParts.length > 2)
-    document.domain = allDomainsParts[allDomainsParts.length - 2] + "." + allDomainsParts[allDomainsParts.length - 1];
-    
-  console.log(document.domain);
+    document.domain = allDomainsParts[allDomainsParts.length - 2] + "." + allDomainsParts[allDomainsParts.length - 1];    
 }
