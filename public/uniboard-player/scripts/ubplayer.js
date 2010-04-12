83941c4739a99afe7eac035e4cfb0a54b7fa5f39
matthieu.rudaz <matthieu.rudaz@mnemis.com>
2010-04-12 14:43:06 +0200

    set document.domain if undefined

--- public/uniboard-player/scripts/ubplayer.js
+++ public/uniboard-player/scripts/ubplayer.js
@@ -1,8 +1,12 @@
 var UbPlayer = {};
 
 UbPlayer.reduceDomain = function(){
+  
+  if(typeof(document.domain) === "undefined")
+    document.domain = "getuniboard.com";
+  
   var allDomainsParts = document.domain.split(".");
   
   if (allDomainsParts.length > 2)
-    document.domain = allDomainsParts[allDomainsParts.length - 2] + "." + allDomainsParts[allDomainsParts.length - 1];    
+    document.domain = allDomainsParts[allDomainsParts.length - 2] + "." + allDomainsParts[allDomainsParts.length - 1];
 }
