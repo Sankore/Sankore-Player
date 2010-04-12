208ab52737acfb1c841213c64f3c8c6a854b8956
matthieu.rudaz <matthieu.rudaz@mnemis.com>
2010-04-12 15:30:16 +0200

    update app viewer

--- public/uniboard-player/scripts/ubplayer.viewer.js
+++ public/uniboard-player/scripts/ubplayer.viewer.js
@@ -6,35 +6,19 @@ UbPlayer.Viewer = function() {
   jQuery("#app-viewer").click(function(){
     that.hide();
   });
-};
-
-UbPlayer.Viewer.prototype.show = function(appUrl){  
-  var appData = [];
-  var app = {
-    index:"", 
-    config:appUrl + "/config.xml", 
-    icon:appUrl + "/icon.png",
-    width:0,
-    height:0,
-    data:{
-      name:"",
-      author:"",
-      description:""
-    }
-  };
-    
-  /*jQuery.ajax({
-    url: app.config,
-    dataType: 'xml',
-    success: function(data){
-      alert("vshow 2");
-      app.index = appUrl + "/" + jQuery(data).find("content").attr("src");
-      app.width = parseInt(jQuery(data).find("widget").attr("width"));
-      app.height = parseInt(jQuery(data).find("widget").attr("height"));
-      app.data.name = jQuery(data).find("name").text();
-      app.data.author = jQuery(data).find("author").text();
-      app.data.description = jQuery(data).find("description").text();
-
+  
+  YAHOO.util.CrossFrame.onMessageEvent.subscribe(
+    function (type, args, obj) {
+      var message = args[0];
+      var domain = args[1];
+      var appData = [];
+      var app = {}
+            
+      appData = message.split(",");
+      app.index = app[0];
+      app.width = app[1];
+      app.height = app[2];
+      
       jQuery("#app-viewer-app")
         .attr("src", app.index)
         .width(app.width)
@@ -42,26 +26,15 @@ UbPlayer.Viewer.prototype.show = function(appUrl){
       jQuery("#app-viewer-appborder")
         .width(app.width + 20)
         .height(app.height + 35)
+        
       jQuery("#app-viewer").show();
       jQuery("#app-viewer-app").show();
     }
-  });*/
-  
-  //if(window.location)
-  
-  YAHOO.util.CrossFrame.onMessageEvent.subscribe(
-    function (type, args, obj) {
-      var message = args[0];
-      var domain = args[1];
-      
-      alert(message);
-      
-      appData = message.split(",");
-      
-      alert(appData[0] + ", " + appData[1]);
-    }
   );
-    
+};
+
+UbPlayer.Viewer.prototype.show = function(appUrl){  
+  // Ask for app config.xml
   YAHOO.util.CrossFrame.send(
     "http://assets.getuniboard.com/publishing/proxy/proxy.html",
     "frames['getAppConf']",
